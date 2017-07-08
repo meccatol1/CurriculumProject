1a8edc447b062349813b08276e86baf26b74e6d4
Hyungsung Kim <meccatol@gmail.com>
2017-07-08 12:48:05 +0900

    주석 조금 추가

--- CurriculumProject/CPCustomContainerViewController.h
+++ CurriculumProject/CPCustomContainerViewController.h
@@ -8,6 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
+// Custom Presentation Controller 와 Custom Transition Delegate 예제입니다.
+// definesPresentationContext 예제도 포함되어 있습니다.
+// Storyboard 에서 InitialViewController를 수정해서 테스트 가능합니다.
 @interface CPCustomContainerViewController : UIViewController
 
 @end
