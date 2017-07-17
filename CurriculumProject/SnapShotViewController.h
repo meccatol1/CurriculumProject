1be54e135f58f1794fc33d4b93e2f28f1de22afe
meccatol <hyungsung.kim@campmobile.com>
2017-07-17 12:26:06 +0900

    SnapShotViewController 주석 조금 수정

--- CurriculumProject/SnapShotViewController.h
+++ CurriculumProject/SnapShotViewController.h
@@ -8,18 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-// 앱이 background상태로 가기전에 스냅샷을 찍기 위해 보여주는 View Controller
-// 1 홈버튼을 누를때나
-// 2 폰 잠글때!
-// 3 앱 델리게이트
-// 4
-// 5
-// 6
-// 7
-// 8
-
-
-
+/** 앱이 background상태로 가기전에 스냅샷을 찍기 위해 보여주는 View Controller
+ *  홈버튼을 누를때나 폰 잠글때!
+ *  앱 델리게이트의 applicationDidEnterBackground에서 모달로 띄워준다.
+ *
+ */
 @interface SnapShotViewController : UIViewController
 
 
