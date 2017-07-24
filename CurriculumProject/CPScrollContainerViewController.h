0490b7fbe787049e0f97016301bd0dc9fd1f6018
meccatol <hyungsung.kim@campmobile.com>
2017-07-24 10:18:50 +0900

    ScrollContainer 테스트

---
+++ CurriculumProject/CPScrollContainerViewController.h
@@ -0,0 +1,13 @@
+//
+//  CPScrollContainerViewController.h
+//  CurriculumProject
+//
+//  Created by Hyungsung_Kim on 2017. 7. 21..
+//  Copyright © 2017년 meccatol_iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPScrollContainerViewController : UIViewController
+
+@end
